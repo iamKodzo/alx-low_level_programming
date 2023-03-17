5ca3ac2910152925e4420c4814df5cd33278f6d6
Jerry Ansah-Ayim <jerryansahayim@gmail.com>
2023-03-16 19:35:28 -0700

    4. puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/*
- * main - print using puts function
+/**
+ * main -Print using puts function
  *
- * Return value: 0
- */
-int main (void)
+ * Return: 0
+*/
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
